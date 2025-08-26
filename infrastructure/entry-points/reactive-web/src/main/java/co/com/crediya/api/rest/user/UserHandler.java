6315d8814d814bf0d3173a50e2ab000500203ed7
Juan Gerardo <jmendez1025@cue.edu.co>
2025-08-25 20:30:47 -0500

    refactor: routes and handler with enums

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserHandler.java
@@ -1,18 +1,14 @@
 package co.com.crediya.api.rest.user;
 
 import co.com.crediya.api.rest.user.dto.CreateApplicantRequestDto;
+import co.com.crediya.api.rest.user.dto.UserResponseDto;
 import co.com.crediya.api.rest.user.mapper.UserRequestMapper;
 import co.com.crediya.api.rest.user.mapper.UserResponseMapper;
 import co.com.crediya.api.utils.ObjectValidator;
 import co.com.crediya.usecase.user.UserUseCase;
-import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.server.ServerRequest;
-import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -25,18 +21,10 @@ public class UserHandler {
     private final UserResponseMapper responseMapper;
     private final ObjectValidator validator;
 
-    public Mono<ServerResponse> createApplicant(ServerRequest request) {
-        return request.bodyToMono(CreateApplicantRequestDto.class)
-                .doOnNext(dto -> log.info(dto.toString()))
-                .flatMap(validator::validate)
+    public Mono<UserResponseDto> createApplicant(CreateApplicantRequestDto dto) {
+        return validator.validate(dto)
                 .map(requestMapper::toDomain)
                 .flatMap(userUseCase::createApplicant)
-                .map(responseMapper::toDto)
-                .flatMap(userCreatedDto ->
-                        ServerResponse
-                                .status(HttpStatus.CREATED)
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .bodyValue(userCreatedDto)
-                );
+                .map(responseMapper::toDto);
     }
 }
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserRoute.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserRoute.java
@@ -1,17 +1,10 @@
 package co.com.crediya.api.rest.user;
 
 import co.com.crediya.api.rest.user.dto.CreateApplicantRequestDto;
-import co.com.crediya.api.rest.user.dto.UserResponseDto;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import org.springdoc.core.annotations.RouterOperation;
-import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -20,46 +13,19 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class UserRoute {
 
-    private final String ENDPOINT_PREFFIX = "/api/v1/users";
-
     @Bean
-    @RouterOperations({
-            @RouterOperation(
-                    path = "/api/v1/users/create-applicant",
-                    method = RequestMethod.POST,
-                    beanClass = UserHandler.class,
-                    beanMethod = "createApplicant",
-                    operation = @Operation(
-                            operationId = "createApplicant",
-                            summary = "Crear solicitante",
-                            description = "Registra un nuevo solicitante en el sistema",
-                            tags = {"Usuarios"},
-                            requestBody = @RequestBody(
-                                    required = true,
-                                    content = @Content(
-                                            mediaType = "application/json",
-                                            schema = @Schema(implementation = CreateApplicantRequestDto.class)
-                                    )
-                            ),
-                            responses = {
-                                    @ApiResponse(
-                                            responseCode = "201",
-                                            description = "Solicitante creado",
-                                            content = @Content(
-                                                    mediaType = "application/json",
-                                                    schema = @Schema(implementation = UserResponseDto.class)
-                                            )
-                                    ),
-                                    @ApiResponse(responseCode = "400", description = "Error de validación"),
-                                    @ApiResponse(responseCode = "500", description = "Error interno")
-                            }
-                    )
-            )
-    })
     public RouterFunction<ServerResponse> userRoutes(UserHandler handler) {
         return route()
-                .path(ENDPOINT_PREFFIX, builder -> builder
-                        .POST("/create-applicant", handler::createApplicant))
+                .path(UserEndpoint.BASE.getPath(), builder -> builder
+                        .POST(UserEndpoint.CREATE_APPLICANT.getPath(), req ->
+                                req.bodyToMono(CreateApplicantRequestDto.class)
+                                        .flatMap(handler::createApplicant)
+                                        .flatMap(dto -> ServerResponse
+                                                .status(HttpStatus.CREATED)
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .bodyValue(dto)
+                                        )
+                        ))
                 .build();
     }
 }
