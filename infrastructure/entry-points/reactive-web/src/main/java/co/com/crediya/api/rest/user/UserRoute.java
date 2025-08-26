dabb8f728a365b9c1f06d0fc801581c5493b9e06
Juan Gerardo <jmendez1025@cue.edu.co>
2025-08-25 22:36:47 -0500

    refactor: router docs with consumer builder

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserRoute.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/api/rest/user/UserRoute.java
@@ -1,6 +1,9 @@
 package co.com.crediya.api.rest.user;
 
+import co.com.crediya.api.rest.user.config.UserOpenApiConfig;
+import co.com.crediya.api.rest.user.constant.UserEndpoint;
 import co.com.crediya.api.rest.user.dto.CreateApplicantRequestDto;
+import org.springdoc.webflux.core.fn.SpringdocRouteBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
@@ -8,24 +11,24 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class UserRoute {
 
     @Bean
     public RouterFunction<ServerResponse> userRoutes(UserHandler handler) {
-        return route()
-                .path(UserEndpoint.BASE.getPath(), builder -> builder
-                        .POST(UserEndpoint.CREATE_APPLICANT.getPath(), req ->
-                                req.bodyToMono(CreateApplicantRequestDto.class)
-                                        .flatMap(handler::createApplicant)
-                                        .flatMap(dto -> ServerResponse
-                                                .status(HttpStatus.CREATED)
-                                                .contentType(MediaType.APPLICATION_JSON)
-                                                .bodyValue(dto)
-                                        )
-                        ))
+        return SpringdocRouteBuilder.route()
+                .POST(UserEndpoint.CREATE_APPLICANT.getPath(),
+                        req -> req.bodyToMono(CreateApplicantRequestDto.class)
+                                .flatMap(handler::createApplicant)
+                                .flatMap(dto ->
+                                        ServerResponse
+                                            .status(HttpStatus.CREATED)
+                                            .contentType(MediaType.APPLICATION_JSON)
+                                            .bodyValue(dto)
+                                ),
+                        ops -> UserOpenApiConfig.createApplicantDocsConsumer().accept(ops)
+                )
                 .build();
     }
 }
